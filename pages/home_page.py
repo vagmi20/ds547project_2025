e8044b2ff552ab184c5f32cfa6a2813268bc90b7
vagmi20 <nagavagmi.bh@gmail.com>
2025-12-08 16:55:52 -0500

    deleted test connection and db

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,10 +4,6 @@ from utils.text import tokenize, remove_stopwords, stemming
 from utils.db import get_connection
 import pandas as pd
 
-conn = get_connection()
-df = pd.read_sql_query("SELECT * FROM mytable LIMIT 5000;", conn)
-print(df.head())
-
 def home_page():
 
     # Create columns for main area and sidebar
--- test.py
+++
@@ -1,7 +0,0 @@
-from utils import db
-import pandas as pd
-
-
-conn = db.get_connection()
-df = pd.read_sql_query("SELECT * FROM mytable LIMIT 5000;", conn)
-print(df.head())
