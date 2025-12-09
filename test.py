01c70d007bee4ce7ec50419a65045d921d4dc46a
ajay-bhagava <ajay.bhagava@gmail.com>
2025-12-09 17:12:29 -0500

    fixed filepath csv path

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ from utils import db
 import pandas as pd
 
 
-db.setup_database("data/song_lyrics.csv")
+db.setup_database("data/song_lyrics_subset.csv")
 conn = db.get_connection()
 df = pd.read_sql_query("SELECT * FROM mytable LIMIT 5000;", conn)
 print(df.head())
 
 ranked = db.bm25("love")
 print(ranked.head())
+
+print(db.query(artist="Kanye West"))
